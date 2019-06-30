5f2fae8d1bb16d7f13fdbfe9a60d18d29f4aa63b
diman <diman@mail.ru>
2019-06-30 17:28:37 +0300

    Separate BottomNavBar

--- lib/wiki_attr_list/attractionsWiki.dart
+++ lib/wiki_attr_list/attractionsWiki.dart
@@ -1,5 +1,6 @@
 import 'package:art_guide_flutter/components/bottom_nav_menu.dart';
 import 'package:art_guide_flutter/ui/colors.dart';
+import './wiki_list_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
@@ -14,40 +15,7 @@ class ListPage extends StatefulWidget {
   }
 }
 
-class ListPageSate extends State<ListPage>
-    with TickerProviderStateMixin<ListPage> {
-  AnimationController _hide;
-
-  @override
-  void initState() {
-    super.initState();
-    _hide = AnimationController(vsync: this, duration: kThemeAnimationDuration);
-  }
-
-  @override
-  void dispose() {
-    _hide.dispose();
-    super.dispose();
-  }
-
-  bool _handleScrollNotification(ScrollNotification notification) {
-    if (notification.depth == 0) {
-      if (notification is UserScrollNotification) {
-        final UserScrollNotification userScroll = notification;
-        switch (userScroll.direction) {
-          case ScrollDirection.forward:
-            _hide.forward();
-            break;
-          case ScrollDirection.reverse:
-            _hide.reverse();
-            break;
-          case ScrollDirection.idle:
-            break;
-        }
-      }
-    }
-    return false;
-  }
+class ListPageSate extends State<ListPage> {
 
   @override
   Widget build(BuildContext context) {
@@ -56,14 +24,11 @@ class ListPageSate extends State<ListPage>
           AppColors.colorGreenStatus, //or set color with: Color(0xFF0000FF)
     ));
 
-    return NotificationListener<ScrollNotification>(
-      onNotification: _handleScrollNotification,
-      child: Scaffold(
-        /*appBar: AppBar(
-          title: Text('Культурный гид'),
-          backgroundColor: Colors.teal,
-        ),*/
+    BottomNavBar  bottomNavBar = BottomNavBar();
 
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notificaton) => bottomNavBar.navBarState._handleScrollNotification(notificaton),
+      child: Scaffold(
         body: new CustomScrollView(slivers: <Widget>[
           const SliverAppBar(
             title: Align(
@@ -87,122 +52,74 @@ class ListPageSate extends State<ListPage>
               ),
               child: SliverList(
                   delegate:
-                      SliverChildListDelegate(buildTextViews(50, context))))
+                      SliverChildListDelegate(WikiListBuilder.buildTextViews(50, context))))
         ]),
-        bottomNavigationBar: ClipRect(
-          child: SizeTransition(
-            sizeFactor: _hide,
-            axisAlignment: -1.0,
-            child: BottomAppBar(
-              /* currentIndex: _currentIndex,
-              onTap: (int index) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              },*/
-
-              child: BottomNavigationMenu(),
-            ),
-          ),
-        ),
+        bottomNavigationBar: bottomNavBar,
       ),
     );
   }
 }
 
-List buildTextViews(int count, BuildContext context) {
-  var longText =
-      'Description Description Description Description Description Description Description Description Description Description Description DescriptionDescription';
+class BottomNavBar extends StatefulWidget {
+  BottomNavBarState navBarState;
 
-  List<Widget> strings = List();
-  for (int i = 0; i < count; i++) {
-    var card = Container(
-      margin: EdgeInsets.only(bottom: 20, right: 20),
-      height: 120,
-      foregroundDecoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          borderRadius: new BorderRadius.only(
-              bottomRight: const Radius.circular(40.0),
-              topRight: const Radius.circular(40.0))),
-      child: Material(
-        borderOnForeground: false,
-        child: InkWell(
-          splashColor: AppColors.colorGreen,
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(40), bottomRight: Radius.circular(40)),
-          onTap: () {},
-          child: Row(
-            children: <Widget>[
-              // ICON
-              Image.asset(
-                'images/memorial_kirovu_small.jpg',
-                width: MediaQuery.of(context).size.width * 0.3,
-                fit: BoxFit.fill,
-              ),
+  @override
+  State<StatefulWidget> createState() {
+    navBarState = BottomNavBarState();
+    return navBarState;
+  }
+}
 
-              // TEXT
-              Column(
-                children: <Widget>[
-                  // TITLE
-                  SizedBox(
-                    height: 40.0,
-                    width: 220.0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Text(
-                        'Title Title Title Title Title Title Title Title Title Title Title Title Title',
-                        maxLines: 2,
-                        style: Theme.of(context).textTheme.body2,
-                      ),
-                    ),
-                  ),
-                  // DESCRIPTION
-                  SizedBox(
-                      height: 30.0,
-                      width: 220.0,
-                      child: Text(longText,
-                          maxLines: 2,
-                          style: Theme.of(context).textTheme.body1)),
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5.0),
-                        child: FlatButton(
-                          shape: StadiumBorder(),
-                          color: AppColors.colorGreen,
-                          onPressed: () {},
-                          child: Text('Show more',
-                              style: TextStyle(color: Colors.white)),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'на карте',
-                          style: TextStyle(
-                              color: AppColors.colorGreen,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Container(
-                          child: Image.asset('images/build_road_red.png'),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
+class BottomNavBarState extends State<BottomNavBar> with TickerProviderStateMixin<BottomNavBar> {
+  AnimationController _hide;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: SizeTransition(
+        sizeFactor: _hide,
+        axisAlignment: -1.0,
+        child: BottomAppBar(
+          child: BottomNavigationMenu(),
         ),
       ),
     );
-    strings.add(card);
   }
-  return strings;
+
+  @override
+  void dispose() {
+    _hide.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _hide = AnimationController(vsync: this, duration: kThemeAnimationDuration);
+  }
+
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (notification.depth == 0) {
+      if (notification is UserScrollNotification) {
+        final UserScrollNotification userScroll = notification;
+        switch (userScroll.direction) {
+          case ScrollDirection.forward:
+            _hide.forward();
+            break;
+          case ScrollDirection.reverse:
+            _hide.reverse();
+            break;
+          case ScrollDirection.idle:
+            break;
+        }
+      }
+    }
+    return false;
+  }
+
 }
+
 
 void main() {
   runApp(MaterialApp(home: ListPage()));
---
+++ lib/wiki_attr_list/wiki_list_builder.dart
@@ -0,0 +1,98 @@
+import 'package:art_guide_flutter/ui/colors.dart';
+import 'package:flutter/material.dart';
+
+class WikiListBuilder {
+  static List buildTextViews(int count, BuildContext context) {
+    var longText =
+        'Description Description Description Description Description Description Description Description Description Description Description DescriptionDescription';
+
+    List<Widget> strings = List();
+    for (int i = 0; i < count; i++) {
+      var card = Container(
+        margin: EdgeInsets.only(bottom: 20, right: 20),
+        height: 120,
+        foregroundDecoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: new BorderRadius.only(
+                bottomRight: const Radius.circular(40.0),
+                topRight: const Radius.circular(40.0))),
+        child: Material(
+          borderOnForeground: false,
+          child: InkWell(
+            splashColor: AppColors.colorGreen,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(40), bottomRight: Radius.circular(40)),
+            onTap: () {},
+            child: Row(
+              children: <Widget>[
+                // ICON
+                Image.asset(
+                  'images/memorial_kirovu_small.jpg',
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  fit: BoxFit.fill,
+                ),
+
+                // TEXT
+                Column(
+                  children: <Widget>[
+                    // TITLE
+                    SizedBox(
+                      height: 40.0,
+                      width: 220.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          'Title Title Title Title Title Title Title Title Title Title Title Title Title',
+                          maxLines: 2,
+                          style: Theme.of(context).textTheme.body2,
+                        ),
+                      ),
+                    ),
+                    // DESCRIPTION
+                    SizedBox(
+                        height: 30.0,
+                        width: 220.0,
+                        child: Text(longText,
+                            maxLines: 2,
+                            style: Theme.of(context).textTheme.body1)),
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5.0),
+                          child: FlatButton(
+                            shape: StadiumBorder(),
+                            color: AppColors.colorGreen,
+                            onPressed: () {},
+                            child: Text('Show more',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'на карте',
+                            style: TextStyle(
+                                color: AppColors.colorGreen,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Container(
+                            child: Image.asset('images/build_road_red.png'),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+      strings.add(card);
+    }
+    return strings;
+  }
+}
