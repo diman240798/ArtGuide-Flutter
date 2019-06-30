5a2591d3c3046f8e9e8423b93cf1032cfc4443e0
diman <diman@mail.ru>
2019-06-30 17:06:01 +0300

    Final ui fix

--- lib/wiki_attr_list/attractionsWiki.dart
+++ lib/wiki_attr_list/attractionsWiki.dart
@@ -111,77 +111,91 @@ class ListPageSate extends State<ListPage>
 }
 
 List buildTextViews(int count, BuildContext context) {
-  var longText = 'Description Description Description Description Description Description Description Description Description Description Description DescriptionDescription';
+  var longText =
+      'Description Description Description Description Description Description Description Description Description Description Description DescriptionDescription';
 
   List<Widget> strings = List();
   for (int i = 0; i < count; i++) {
     var card = Container(
       margin: EdgeInsets.only(bottom: 20, right: 20),
       height: 120,
-      decoration: BoxDecoration(
-          color: Colors.white,
+      foregroundDecoration: BoxDecoration(
           border: Border.all(color: Colors.black),
           borderRadius: new BorderRadius.only(
               bottomRight: const Radius.circular(40.0),
               topRight: const Radius.circular(40.0))),
-      child: InkWell(
-        onTap: () {},
-        child: Row(
-          children: <Widget>[
-            // ICON
-            Image.asset(
-              'images/memorial_kirovu_small.jpg',
-              width: MediaQuery.of(context).size.width * 0.3,
-              fit: BoxFit.fill,
-            ),
+      child: Material(
+        borderOnForeground: false,
+        child: InkWell(
+          splashColor: AppColors.colorGreen,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(40), bottomRight: Radius.circular(40)),
+          onTap: () {},
+          child: Row(
+            children: <Widget>[
+              // ICON
+              Image.asset(
+                'images/memorial_kirovu_small.jpg',
+                width: MediaQuery.of(context).size.width * 0.3,
+                fit: BoxFit.fill,
+              ),
 
-            // TEXT
-            Column(
-              children: <Widget>[
-                // TITLE
-                SizedBox(
-                  height: 40.0,
-                  width: 220.0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      'Title Title Title Title Title Title Title Title Title Title Title Title Title',
-                      maxLines: 2,
-                      style: Theme.of(context).textTheme.body2,
-                    ),
-                  ),
-                ),
-                // DESCRIPTION
-                SizedBox(
-                    height: 30.0,
+              // TEXT
+              Column(
+                children: <Widget>[
+                  // TITLE
+                  SizedBox(
+                    height: 40.0,
                     width: 220.0,
-                    child: Text(
-                        longText,
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        'Title Title Title Title Title Title Title Title Title Title Title Title Title',
                         maxLines: 2,
-                        style: Theme.of(context).textTheme.body1)),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5.0),
-                      child: FlatButton(
-                        shape: StadiumBorder(),
-                        color: AppColors.colorGreen,
-                        onPressed: () {},
-                        child:
-                            Text('Show more', style: TextStyle(color: Colors.white)),
+                        style: Theme.of(context).textTheme.body2,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('на карте', style: TextStyle(color: AppColors.colorGreen, fontWeight: FontWeight.bold),),
-                    ),
-                    IconButton(onPressed: () {}, icon: Container(child: Image.asset('images/build_road_red.png'),),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ],
+                  ),
+                  // DESCRIPTION
+                  SizedBox(
+                      height: 30.0,
+                      width: 220.0,
+                      child: Text(longText,
+                          maxLines: 2,
+                          style: Theme.of(context).textTheme.body1)),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5.0),
+                        child: FlatButton(
+                          shape: StadiumBorder(),
+                          color: AppColors.colorGreen,
+                          onPressed: () {},
+                          child: Text('Show more',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'на карте',
+                          style: TextStyle(
+                              color: AppColors.colorGreen,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Container(
+                          child: Image.asset('images/build_road_red.png'),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
